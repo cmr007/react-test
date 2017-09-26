ed5674d6f6bbc7a687e75b6f08148cb6f37a04ff
kally <kally@hyx.com>
2017-09-26 18:34:19 +0800

    Signed-off-by: kally <kally@hyx.com>

--- src/static/webapps/js/views/home/main.js
+++ src/static/webapps/js/views/home/main.js
@@ -6,75 +6,80 @@ import Pic_03 from '../../../image/Home/img-3.jpg'
 import Pic_04 from '../../../image/Home/img-4.jpg'
 import Pic_05 from '../../../image/Home/img-5.jpg'
 import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
 class Home extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
+    this.handle = this.handle.bind(this)
+    this.state 
+  }
+  handle() {
+  	console.info("哈哈哈")
   }
   render() {
   	var settings = {
-    	dots: true,
-    	dotsClass: 'gallery',
-    	autoplay: true,
+    	dots: false,
+    	autoplay: false,
     	infinite: true,
     	arrows: false,
       slidesToShow: 1,
       slidesToScroll: 1,
       centerMode: true,
       speed: 300,
-      customPaging: function(i) {
-      	return <a><img src={Pic_05}/></a>
-        
-      },
+      beforeChange: (next)=>{
+      	console.info(next)
+      }
     }
-    return (<div className="home">
+    return (<div id="home">
       <Slider {...settings}>
         <div>
-        	<div className='pic-list'>
+        	<div className='pic-list' onClick={this.handle}>
         		<img src={Pic_01}/>
-        		<div>
+        		<div className='sku'>
         			<div className="detail"><span className="price">￥119.00</span><span className='name'>稻草人手提包</span></div>
-        			<div><i className="iconfont icon-gouwuche"></i></div>
+        			<div className="shopping-cart"><i className="iconfont icon-gouwuche"></i></div>
         		</div>
         	</div>
         </div>
         <div>
         	<div className='pic-list'>
         		<img src={Pic_02}/>
-        		<div>
-        			<div><span>￥219.00</span><span>稻草人手提包</span></div>
-        			<div><i className="iconfont icon-gouwuche"></i></div>
+        		<div className='sku'>
+        			<div className="detail"><span className="price">￥219.00</span><span>稻草人手提包</span></div>
+        			<div className="shopping-cart"><i className="iconfont icon-gouwuche"></i></div>
         		</div>        	
         	</div>
         </div>
         <div>
         	<div className='pic-list'>
         		<img src={Pic_03}/>
-        		<div>
-        			<div><span>￥319.00</span><span>稻草人手提包</span></div>
-        			<div><i className="iconfont icon-gouwuche"></i></div>
+        		<div className='sku'>
+        			<div className="detail"><span className="price">￥319.00</span><span>稻草人手提包</span></div>
+        			<div className="shopping-cart"><i className="iconfont icon-gouwuche"></i></div>
         		</div>
         	</div>
         </div>
         <div>
-        	<div  className='pic-list'>
-        		<img src={Pic_04}/>
-        		<div>
-        			<div><span>￥419.00</span><span>稻草人手提包</span></div>
-        			<div><i className="iconfont icon-gouwuche"></i></div>
-        		</div>
-        	</div>
+      	<div className='pic-list'>
+      		<img src={Pic_04}/>
+      		<div className='sku'>
+      			<div className="detail"><span className="price">￥419.00</span><span>稻草人手提包</span></div>
+      			<div className="shopping-cart"><i className="iconfont icon-gouwuche"></i></div>
+      		</div>
+      	</div>
         	
         </div>
         <div>
         	<div className='pic-list'>
         		<img src={Pic_05}/>
-        		<div>
-        			<div><span>￥519.00</span><span>稻草人手提包</span></div>
-        			<div><i className="iconfont icon-gouwuche"></i></div>
+        		<div className='sku'>
+        			<div className="detail"><span className="price">￥519.00</span><span>稻草人手提包</span></div>
+        			<div className="shopping-cart"><i className="iconfont icon-gouwuche"></i></div>
         		</div>
         	</div>
         </div>
       </Slider>
+      <div id='mask'></div>
     </div>)
   }
 }
